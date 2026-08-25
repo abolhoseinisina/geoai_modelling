b14a4f3279c1a8ff767bc6bc163c0aa1bce8aaed
abolhoseinisina <abolhoseini.sina@gmail.com>
2026-08-25 09:08:20 -0500

    change download to boto3

--- training_dataset/download.py
+++ training_dataset/download.py
@@ -1,21 +1,34 @@
 import os
+import boto3
 import zipfile
-import subprocess
+from botocore.exceptions import NoCredentialsError, ClientError
 
-S3_BUCKET = "s3://fm-ca-assets/geoai/"
-ZIP_FILENAMES = ["building_segmentation_202608.zip"]
+S3_BUCKET = 'fm-ca-assets'
+S3_PREFIX = 'geoai'
+ZIP_FILENAMES = ['building_segmentation_202608.zip']
 
 def downloadFromS3(zip_filename, zip_path):
-    print(f"Downloading {zip_filename} from S3")
-    result = subprocess.run(["aws", "s3", "cp", S3_BUCKET + zip_filename, zip_path], check=True)
-    print("Download complete.")
+    s3 = boto3.client('s3')
+    key = f'{S3_PREFIX}/{zip_filename}'
+    try:
+        print(f'Downloading "{zip_filename}" from S3 bucket "{S3_BUCKET}"')
+        s3.download_file(S3_BUCKET, key, zip_path)
+        print('Download complete.')
+
+    except NoCredentialsError:
+        print('AWS credentials not found. Please configure your AWS credentials.')
+        raise
+    
+    except ClientError as e:
+        print(f'Error downloading file from S3: {e}')
+        raise
 
 def extractZip(zip_filename, zip_path):
-    print(f"Extracting {zip_filename}")
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+    print(f'Extracting "{zip_filename}"')
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(os.getcwd())
 
-    print("Extraction complete.")
+    print('Extraction complete.')
 
 def main():
     for zip_filename in ZIP_FILENAMES:
@@ -24,9 +37,9 @@ def main():
             downloadFromS3(zip_filename, zip_path)
         
         else:
-            print(f"{zip_filename} already exists. Skipping download.")
+            print(f'{zip_filename} already exists. Skipping download.')
         
         extractZip(zip_filename, zip_path)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
