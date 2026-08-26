1efb0f3c8182935c40e8c7bce95a05dc9318d1f1
abolhoseinisina <abolhoseini.sina@gmail.com>
2026-08-26 10:04:44 -0500

    clean up

--- create/building_segmentation/fine_tune/run.py
+++ create/building_segmentation/fine_tune/run.py
@@ -6,7 +6,7 @@ import generate_finetuned_model
 
 def main():
     config = parseConfig()
-    print(f"Using '{config.name}' config: device={config.device}  epochs={config.epochs}  batch={config.batch_size}  lr={config.learning_rate}")
+    print(f"Config: '{config.name}', device:{config.device}, epochs:{config.epochs}, batch:{config.batch_size}, lr:{config.learning_rate}")
     
     generate_tiles.execute()
     generate_finetuned_model.execute(config)
